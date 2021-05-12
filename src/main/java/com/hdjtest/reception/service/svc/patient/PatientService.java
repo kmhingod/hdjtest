4ecfab89916efdd9c32b675b2bc6dd4a3eb0872d
kmhingod <kmhingod@gmail.com>
2021-05-12 14:37:08 +0900

    환자 관련 기본 API 구현 처리
    1. 환자 등록 시
      1) 환자등록번호 병원별로 중복되지 않도록 기능 추가
      2) 신규 환자의 경우 YYYY + 000000000 형식으로 총 13자리 모두 사용
    
    2. 환자 수정 시
      1) 이미 구현 해놓은 상태라서 변경 없음
    
    3. 환자 삭제 시
      1) 이미 구현 해놓은 상태라서 변경 없음
    
    4. 환자 정보 조회
      1) 환자id로 조회 기능에 Entity 정보(기 구현)와 내원 정보를 함께 조회할 수 있도록 변경
    
    5. 환자 목록 조회
      1) 기획서에 따라 환자명, 환자등록번호, 생년월일 별로 조회 기능 추가
      2) 추가로 환자 목록 조회 시 내원 정보도 같이 보내는 api 별도 생성
      3) 전달 컬럼들은 우선 전체를 보내고 추후 상황에 따라 별도 Dto 작성 필요
    
    6. 기타
      1) 리베이스 실수로 지워졌던 application.properties 원복
      2) 테스트용 기초 데이터 생성 변경 : 환자등록번호 포맷 맞춤
    
    :TODO
    - DTO를 어느 정도까지 세분화 해야할 지 아직은 좀 아리까리함.

--- src/main/java/com/hdjtest/reception/service/svc/patient/PatientService.java
+++ src/main/java/com/hdjtest/reception/service/svc/patient/PatientService.java
@@ -4,11 +4,16 @@ import com.hdjtest.reception.domain.base.Hospital;
 import com.hdjtest.reception.domain.base.HospitalRepository;
 import com.hdjtest.reception.domain.patient.Patient;
 import com.hdjtest.reception.domain.patient.PatientRepository;
+import com.hdjtest.reception.domain.patient.VisitRepository;
 import com.hdjtest.reception.service.dto.patient.PatientDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,16 +25,95 @@ public class PatientService {
     // 2021.05.12 김민형 - 환자 저장 전에 병원 존재여부 확인 위해.
     private final HospitalRepository hospitalRepository;
 
+    private final VisitRepository visitRepository;
+
     @Transactional(readOnly = true)
-    public PatientDto selectById(Long patientId) {
+    public PatientDto selectById(Long patientId, Boolean includeVisitList) {
         Patient patient = patientRepository.findById(patientId)
                 .orElseThrow(() -> new IllegalArgumentException("환자가 존재하지 않습니다. 환자ID : " + patientId));
 
-        return new PatientDto(patient);
+        PatientDto patientDto = new PatientDto(patient);
+
+        if (includeVisitList) {
+            // 2021.05.12 김민형 - Repository에서 받아오는 것도 해봤는데 Patient Entity에서 Onetomany 관계된 것으로 가져오는것도 되는군.
+            //patientDto.setVisitList((patientDto.ConvertVisitEntityListToDtoList(visitRepository.selectVisitsByPatientId(patientId))));
+            patientDto.setVisitList((patientDto.ConvertVisitEntityListToDtoList(patient.getVisitList())));
+        }
+
+        return patientDto;
     }
+
+    @Transactional(readOnly = true)
+    public List<PatientDto> selectDtoAll(String searchType, String searchValue, Boolean includeVisitList) {
+        List<PatientDto> patientDtoList = new ArrayList<>();
+        List<Patient> patientList = new ArrayList<Patient>();
+
+        System.out.println(">>>> request params : " +  searchType + ", " +  searchValue);
+        if (searchType.equals("patient_name")) {
+            patientList = patientRepository.selectPatientListByName(searchValue);
+        } else if (searchType.equals("patient_reg_num")) {
+            patientList = patientRepository.selectPatientListByPatientRegNum(searchValue);
+        } else if (searchType.equals("patient_birth")) {
+            patientList = patientRepository.selectPatientListByBirth(searchValue);
+        } else {
+            // 2021.05.12 김민형 - 검색 값이 없는 경우는 반환할게 없다.
+            //patientList = patientRepository.findAll();
+        }
+
+        System.out.println(">>>> patientList Count : " +  patientList.size());
+        // 2021.05.12 김민형 - Mapper 사용을 해야하는데...나중에 리펙토링 필요하다.
+        for(Patient patient : patientList) {
+            PatientDto patientDto = new PatientDto(patient);
+
+            if (includeVisitList) {
+                patientDto.setVisitList((patientDto.ConvertVisitEntityListToDtoList(patient.getVisitList())));
+            }
+
+            patientDtoList.add(patientDto);
+        }
+
+        return patientDtoList;
+    }
+
+    // 2021.05.12 김민형 - 방문목록 포함하는 파라미터 추가하여 함수 하나로 사용
+//    @Transactional(readOnly = true)
+//    public PatientDto selectByIdWithVisits(Long patientId) throws Exception {
+//        System.out.println(">>>> parameter id : " + patientId);
+//
+//        Patient patient = patientRepository.findById(patientId)
+//                .orElseThrow(() -> new IllegalArgumentException("환자가 존재하지 않습니다. 환자ID : " + patientId));
+//
+//        PatientDto patientDto = new PatientDto(patient);
+//
+//        // 2021.05.12 김민형 - Repository에서 받아오는 것도 해봤는데 Patient Entity에서 Onetomany 관계된 것으로 가져오는것도 되는군.
+//       //patientDto.setVisitList((patientDto.ConvertVisitEntityListToDtoList(visitRepository.selectVisitsByPatientId(patientId))));
+//        patientDto.setVisitList((patientDto.ConvertVisitEntityListToDtoList(patient.getVisitList())));
+//
+//        return patientDto; //new PatientDto(patient);
+//    }
 
     @Transactional
     public Long save(PatientDto patientDto) throws Exception {
+
+        String lastRegNum = patientRepository.getMaxPatientRegNum(patientDto.getHospId());
+        System.out.println(">>>>> searchedRegNum : " + lastRegNum);
+
+        // 2021.05.13 김민형 - 우선 화면 단에서 환자등록번호를 가져오고 존재하는 경우 기존 것을 사용하고
+        //                    없는 경우 새로 생성한다.
+        //                    환자등록번호는 YYYY+9자리 시퀸스로 한다.
+        //                    최종 환자등록번호와 다른 번호들은 별도 테이블로 만들어서 관리하는 것이 좋을 것 같다.
+        if (lastRegNum == null) {
+            String newRegNum = LocalDate.now().getYear() + String.format("%08d%d", 0, 1);
+            System.out.println(">>>>> lastRegNum : " + newRegNum);
+            patientDto.setPatientRegNum(newRegNum);
+        } else if (patientDto.getPatientRegNum() != lastRegNum) {
+            Integer newNum = Integer.parseInt(lastRegNum.substring(4)) + 1;
+            String newRegNum = lastRegNum.substring(0, 4) + String.format("%09d", newNum);
+
+            System.out.println(">>>>> newRegNum : " + newRegNum);
+            patientDto.setPatientRegNum(newRegNum);
+        }
+
         Patient patient = patientDto.createEntity(patientDto);
         System.out.println(">>>>> Patient Before Save : " + patient);
 
@@ -75,6 +159,7 @@ public class PatientService {
     public PatientDto selectPatientDto(Long patientId) {
         Patient patient = patientRepository.findById(patientId)
                 .orElseThrow(() -> new IllegalArgumentException("환자가 존재하지 않습니다. 환자ID :" + patientId));
+
 
         return new PatientDto(patient);
     }
